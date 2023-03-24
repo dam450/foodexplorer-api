ea50723acc86a3ad5a966ff25058f541016a247f
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-24 18:06:12 -0300

    encrypt password & insert user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,24 @@
+const knex = require('../database/knex');
+const { hash } = require('bcryptjs');
+
 const { AppError } = require('../utils/AppError');
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
     if (!name || !email || !password)
       throw new AppError('name, email e password são obrigatórios', 401);
 
-    response.status(201).json({ name, email, password });
+    //TODO: validate email
+
+    const user = await knex('users').select('*').where({ email }).first();
+    if (user) throw new AppError('Usuário já cadastrado!', 401);
+
+    const hashedPassword = await hash(password, 8);
+    const [newUser] = await knex('users').insert({ name, email, password: hashedPassword });
+
+    response.status(201).json({ userId: newUser });
   }
 }
 
