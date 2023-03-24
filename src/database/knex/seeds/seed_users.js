834a892dc882e399353dd00df5539467e3416878
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-24 18:12:52 -0300

    update user seeds

--- src/database/knex/seeds/seed_users.js
+++ src/database/knex/seeds/seed_users.js
@@ -6,8 +6,18 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del();
   await knex('users').insert([
-    { name: 'John Doe', email: 'john@email.com', password: '123456', admin: false },
-    { name: 'Jane Doe', email: 'jane@email.com', password: '123456', admin: true },
+    {
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: '$2a$08$H8fFI3qparBlFK44JxuGz.k/HEdngmYNRePYZJNgOiIM/3pNuNkKS',
+      admin: false,
+    },
+    {
+      name: 'Jane Doe',
+      email: 'jane@email.com',
+      password: '$2a$08$qxjzyetA.ZNEcKDGz4ChpexlWcNICjfkWrf.3f89YXqzp.Mx4E0zi',
+      admin: true,
+    },
   ]);
 };
 
