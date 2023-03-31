6bdd196e9ba0432355b66c81a9a3fb392082b16f
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-31 15:38:23 -0300

    install cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ require('express-async-errors');
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const { appErrorMiddleware } = require('./utils/AppError');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(appErrorMiddleware);
