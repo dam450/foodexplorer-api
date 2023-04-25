7ce909ef986cb0f8e34246a0daa1fba2dc402035
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-24 23:00:02 -0300

    create dishes routes

---
+++ src/routes/dishes.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const DishesController = require('../controllers/DishesController');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+
+const dishesRoutes = Router();
+const dishesController = new DishesController();
+
+dishesRoutes.use(ensureAuthenticated);
+
+dishesRoutes.post('/', dishesController.create);
+dishesRoutes.put('/:id', dishesController.update);
+dishesRoutes.get('/:id', dishesController.show);
+dishesRoutes.get('/', dishesController.index);
+dishesRoutes.patch('/:id/picture', dishesController.patch);
+
+module.exports = dishesRoutes;
