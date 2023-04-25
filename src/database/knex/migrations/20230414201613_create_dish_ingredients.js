31b060b25ebb5371de9fdf17c9db00d1ee5b6898
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-24 22:56:34 -0300

    create migrations dishes, categories & ingredients

---
+++ src/database/knex/migrations/20230414201613_create_dish_ingredients.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = knex =>
+  knex.schema.createTable('dish_ingredients', table => {
+    table.increments('id');
+    table.integer('dish_id').notNullable().references('id').inTable('dishes').onDelete('CASCADE');
+    table.text('name').notNullable();
+    table.string('name').notNullable();
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable('dish_ingredients');
