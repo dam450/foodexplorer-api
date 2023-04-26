177347b7bd90da723fbe78bb3ebeafede51342f5
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-26 00:48:05 -0300

    make dish picture controller save files

--- src/controllers/DishPictureController.js
+++ src/controllers/DishPictureController.js
@@ -1,17 +1,25 @@
 const knex = require("../database/knex");
+const DiskStorage = require('../providers/DiskStorage');
 
 class DishPictureController {
   async updatePicture(request, response) {
     const { id } = request.params;
-    const { filename } = request.file;
+    const pictureFilename = request.file.filename;
 
-    console.log('file: ', filename);
+    const diskStorage = new DiskStorage();
 
     const dish = await knex('dishes').where({ id }).first();
     if (!dish) throw new AppError('Prato não encontrado', 404);
 
-    //TODO: save dish image
-    return response.json({ filename });
+    if (dish.picture)
+      await diskStorage.deleteFile(dish.picture);
+
+    const filename = await diskStorage.saveFile(pictureFilename);
+    dish.picture = filename;
+
+    await knex('dishes').where({ id }).update(dish);
+
+    return response.json({ picture: filename });
   }
 }
 
