0c03bd2222c7525993e7075588d8f4a9e60e4910
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-26 00:48:39 -0300

    create static route to serve files

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { appErrorMiddleware } = require('./utils/AppError');
+const uploadConfig = require('./configs/upload');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(appErrorMiddleware);
+app.use('/files', express.static(uploadConfig.UPLOAD_FOLDER));
 
 app.listen(PORT, function () {
   console.log(`[Server] listening on port ${PORT}`);
