ee5c27d5237f357e1c544ce9d919c856d542d52e
Dam450 <68165752+dam450@users.noreply.github.com>
2023-05-26 17:39:31 -0300

    separate file routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const usersRoutes = require('./users.routes');
 const sessionsRoutes = require('./sessions.routes');
 const dishesRoutes = require('./dishes.routes');
+const filesRoutes = require('./files.routes');
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/dishes', dishesRoutes);
+routes.use('/files', filesRoutes);
 
 module.exports = routes;
