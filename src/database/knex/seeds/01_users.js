df9fd1ad577c72e9ae55f9d44887834649b569a7
Dam450 <68165752+dam450@users.noreply.github.com>
2023-05-29 20:40:53 -0300

    update user email seeds

--- src/database/knex/seeds/01_users.js
+++ src/database/knex/seeds/01_users.js
@@ -8,13 +8,13 @@ exports.seed = async function (knex) {
   await knex('users').insert([
     {
       name: 'John Doe',
-      email: 'john@email.com',
+      email: 'user@email.com',
       password: '$2a$10$q2u7xgVso1sHzfe4mBdKeu495O17Z307AXgaA9Ou.fBPeBZhGQEBO',
       admin: false,
     },
     {
       name: 'Jane Doe',
-      email: 'jane@email.com',
+      email: 'admin@email.com',
       password: '$2a$10$q2u7xgVso1sHzfe4mBdKeu495O17Z307AXgaA9Ou.fBPeBZhGQEBO',
       admin: true,
     },
