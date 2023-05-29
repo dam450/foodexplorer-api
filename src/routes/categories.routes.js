4317b525605ab15f7a74915265dc62a944fab9d8
Dam450 <68165752+dam450@users.noreply.github.com>
2023-05-29 20:40:17 -0300

    create ensureAdmin

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+
 const CategoriesController = require('../controllers/CategoriesController');
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const ensureAdmin = require('../middlewares/ensureAdmin');
 
 const categoriesRoutes = Router();
 const categoriesController = new CategoriesController();
 
+categoriesRoutes.use(ensureAuthenticated);
 
-categoriesRoutes.get('/', ensureAuthenticated, categoriesController.index);
-categoriesRoutes.post('/', ensureAuthenticated, categoriesController.create);
+categoriesRoutes.get('/', categoriesController.index);
+categoriesRoutes.post('/', ensureAdmin, categoriesController.create);
+categoriesRoutes.delete('/', ensureAdmin, categoriesController.delete);
 
 module.exports = categoriesRoutes;
